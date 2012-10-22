ff22a20149564072a9374939515d51bced8003e3
Bernardo Amorim <bamorim2@gmail.com>
2012-10-22 19:33:23 -0200

    Agora o miner baixa também as questões, também tornei mais modular.

--- data/miner.rb
+++ data/miner.rb
@@ -1,27 +1,33 @@
-require 'open-uri'
+def download(source, target, varname)
+  require 'open-uri'
+  puts "Downloading #{source}..."
+  begin
+    source = open("http://www.fuvest.br/#{source}") { |f| f.read }
+  rescue
+    puts "Can't download #{source}!"
+    return
+  end
+  content = source[source.index(varname)..-1]
+  content = content[content.index("=")+1..-1]
+
+  file = File.new target, "w"
+  file.write content
+  file.close
+end
+
+Dir::mkdir Dir::pwd+"/json" if !FileTest::directory?(Dir::pwd+"/json")
 
 anos = (1995..2012)
 fases = ["inscritos","2a_fase", "1a_chamada", "ultima_chamada"]
 tipos = ["regiao", "carreira", "carreira_campus"]
 
 anos.each do |ano|
-  dir_name = Dir::pwd + "/" + "#{ano}"
+  dir_name = Dir::pwd + "/json/" + "#{ano}"
   Dir::mkdir dir_name if !FileTest::directory?(dir_name)
+  download "vest#{ano}/js/qase_quest_#{ano}.js", "#{dir_name}/questoes.json", "aQst"
   (1..4).each do |fase|
     (1..3).each do |tipo|
-      puts "Downloading qase_grupos_#{fase}_#{tipo}_#{ano}.js..."
-      begin
-        source = open("http://www.fuvest.br/vest#{ano}/js/qase_grupos_#{fase}_#{tipo}_#{ano}.js") { |f| f.read }
-      rescue
-        puts "Can't download qase_grupos_#{fase}_#{tipo}_#{ano}.js!"
-        next
-      end
-      content = source[source.index("aGrp")..-1]
-      content = content[content.index("=")+1..-1]
-
-      file = File.new "#{dir_name}/#{fases[fase-1]}-#{tipos[tipo-1]}.json", "w"
-      file.write content
-      file.close
+      download "vest#{ano}/js/qase_grupos_#{fase}_#{tipo}_#{ano}.js", "#{dir_name}/#{fases[fase-1]}-#{tipos[tipo-1]}.json", "aGrp"
     end
   end
 end
